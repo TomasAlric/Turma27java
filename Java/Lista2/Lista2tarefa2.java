5bcde4e6b6bdef7de76e37bd7c14663d9905a670
unknown <tomas.alricc@gmail.com>
2021-07-03 00:03:23 -0300

    JAVA | Lista 2

--- Java/Lista2/Lista2tarefa2.java
+++ Java/Lista2/Lista2tarefa2.java
@@ -21,8 +21,8 @@ public class Lista2tarefa2 {
 		} else if (a > b && b > c) {
 			System.out.printf(" %d - %d - %d", c, b, a);
 		}
-		  else if (a > c && c > b) {
-			System.out.printf(" %d - %d - %d", a, c, b);
+		  else if (b > a && a > c) {
+			System.out.printf(" %d - %d - %d", c, a, b);
 		}
 	}
 }
