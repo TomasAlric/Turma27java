8f4fa25dafce02138d6a95f3485c10b3fbe7300b
unknown <tomas.alricc@gmail.com>
2021-07-03 00:13:55 -0300

    JAVA | Lista 1

---
+++ java/Lista 1/Lista1tarefa2.java
@@ -0,0 +1,23 @@
+  
+package Exercícios;
+
+import java.util.Scanner;
+
+public class Lista1tarefa2 {
+
+	public static void main(String[] args) {
+		int num, anos, meses, dias;
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Diga um número e vamos converter para anos, meses e dias: ");
+		num = leia.nextInt();
+		
+		anos = (num/365);
+		meses = (num % 365)/30;
+		dias = (num % 365) % 30;
+		
+		System.out.println("Isso totaliza " + anos + " anos, " + meses + " meses, e " + dias + " dias.");
+
+	}
+
+}
---
+++ java/Lista 1/Lista1tarefa4.java
@@ -0,0 +1,30 @@
+package Exercícios;
+
+import java.util.Scanner;
+
+public class Lista1tarefa4 {
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+		
+		double a, b, c, r, s, d;
+		
+		System.out.println("Digite o valor A: ");
+		a = leia.nextDouble();
+		
+		System.out.println("Digite o valor B: ");
+		b = leia.nextDouble();
+		
+		System.out.println("Digite o valor C: ");
+		c = leia.nextDouble();
+		
+		r = Math.pow((a+b), 2);
+		s = Math.pow((b+c), 2);
+		
+		d = ((r+s)/2);
+		
+		System.out.println("Resultado: " + d);
+		
+	}
+
+}
