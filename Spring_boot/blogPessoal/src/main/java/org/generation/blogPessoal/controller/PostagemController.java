62b169b98594c5df5bd4881526cccc39eaa34a61
unknown <tomas.alricc@gmail.com>
2021-07-29 14:16:30 -0300

    BlogPessoal | Parte 2

--- Spring_boot/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ Spring_boot/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,5 +47,9 @@ public class PostagemController {
 	@PutMapping
 	public ResponseEntity<Postagem> Put(@RequestBody Postagem Postagem){
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(Postagem));
+	}
+	@DeleteMapping("/{id}")
+	public void Delete(@PathVariable long id) {
+		repository.deleteById(id);
 	}
 }
